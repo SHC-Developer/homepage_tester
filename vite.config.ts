fd533d91f3dbf5a435db4d08c828f2aacde00158
kyungmin.kim <lufovic77@gmail.com>
2025-10-01 22:28:03 +0900

    test5

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "./",   // 반드시 repo 이름!
+  base: "/homepage_tester/",   // 반드시 repo 이름!
   plugins: [react()],
   resolve: {
     alias: {
